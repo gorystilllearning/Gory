f90e17d3686b8a6f93b511042b6e64299fe0cf75
gory <gory@wiseman-laptop.local>
2026-05-25 15:21:26 +0800

    Update bootmodes to modern systemd-boot to fix grub error

--- profiledef.sh
+++ profiledef.sh
@@ -8,9 +8,7 @@ iso_application="Gory Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
