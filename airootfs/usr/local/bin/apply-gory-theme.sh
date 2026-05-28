813cb2410591ce3ff894b63156d277766c01e3bc
Gory Wiseman <144646383+gorystilllearning@users.noreply.github.com>
2026-05-28 13:11:19 +0800

    Fix root autostart and wallpaper script

--- airootfs/usr/local/bin/apply-gory-theme.sh
+++ airootfs/usr/local/bin/apply-gory-theme.sh
@@ -18,30 +18,8 @@ kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight ""
 # Reconfigure KWin to apply window decoration changes immediately
 qdbus org.kde.KWin /KWin reconfigure
 
-# Configure Panels and Wallpaper via Plasma scripting
-dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript string:"
-var panels = panels();
-if (panels.length > 0) {
-    panels[0].location = 'top';
-    panels[0].height = 24;
-}
-
-var dock = new Panel;
-dock.location = 'bottom';
-dock.height = 48;
-dock.alignment = 'center';
-dock.lengthMode = 'fit';
-dock.floating = true;
-dock.addWidget('org.kde.plasma.icontasks');
-
-var Desktops = desktops();
-for (i=0; i<Desktops.length; i++) {
-    d = Desktops[i];
-    d.wallpaperPlugin = 'org.kde.image';
-    d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');
-    d.writeConfig('Image', 'file:///usr/share/backgrounds/gory-wallpaper.jpg');
-}
-"
+# Set wallpaper via Plasma 6 CLI tool
+plasma-apply-wallpaperimage /usr/share/backgrounds/gory-wallpaper.jpg
 
 # Remove the autostart desktop file so it only runs on first boot
 rm -f ~/.config/autostart/apply-gory-theme.desktop
