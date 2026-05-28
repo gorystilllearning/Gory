4faa912ba8bb4d416470b9a709ad396c8f9e4a69
Gory Wiseman <144646383+gorystilllearning@users.noreply.github.com>
2026-05-28 14:35:51 +0800

    Refine macOS top panel layout

--- airootfs/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/99-gory-layout.js
+++ airootfs/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/99-gory-layout.js
@@ -4,13 +4,22 @@ if (myPanels.length > 0) {
     p.location = "top";
     p.height = 24;
     
-    // Change the launcher icon to a dot (media-record)
-    for (var i = 0; i < p.applets.length; i++) {
-        if (p.applets[i].pluginName === "org.kde.plasma.kickoff") {
+    // Modify applets on the top panel
+    for (var i = p.applets.length - 1; i >= 0; i--) {
+        var plugin = p.applets[i].pluginName;
+        // Change the launcher icon to a dot
+        if (plugin === "org.kde.plasma.kickoff") {
             p.applets[i].currentConfigGroup = ["General"];
             p.applets[i].writeConfig("icon", "media-record");
         }
+        // Remove task managers from top panel
+        if (plugin === "org.kde.plasma.icontasks" || plugin === "org.kde.plasma.taskmanager" || plugin === "org.kde.plasma.pager") {
+            p.applets[i].remove();
+        }
     }
+    
+    // Add global menu next to the kickoff (index 1)
+    p.addWidget("org.kde.plasma.appmenu");
 }
 
 var dock = new Panel("org.kde.panel");
