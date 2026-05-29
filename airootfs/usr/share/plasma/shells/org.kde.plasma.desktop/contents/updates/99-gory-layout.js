b5f67c39ef74ae89ecc785585585fca61b0b40fc
Gory Wiseman <khairiearrantisy51@gmail.com>
2026-05-29 21:49:39 +0800

    Update Plasma layout: pure circle home icon, center clock, right system tray

--- airootfs/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/99-gory-layout.js
+++ airootfs/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/99-gory-layout.js
@@ -6,19 +6,16 @@ for (var i = 0; i < myPanels.length; i++) {
 var topPanel = new Panel("org.kde.panel");
 topPanel.location = "top";
 topPanel.height = 24;
-topPanel.addWidget("org.kde.plasma.kickoff");
-topPanel.addWidget("org.kde.plasma.appmenu");
-topPanel.addWidget("org.kde.plasma.marginsseparator");
-topPanel.addWidget("org.kde.plasma.systemtray");
-topPanel.addWidget("org.kde.plasma.digitalclock");
 
-// configure kickoff to have the media-record icon
-for (var i = 0; i < topPanel.applets.length; i++) {
-    if (topPanel.applets[i].pluginName === "org.kde.plasma.kickoff") {
-        topPanel.applets[i].currentConfigGroup = ["General"];
-        topPanel.applets[i].writeConfig("icon", "media-record");
-    }
-}
+var kickoff = topPanel.addWidget("org.kde.plasma.kickoff");
+kickoff.currentConfigGroup = ["General"];
+kickoff.writeConfig("icon", "radiobutton-unchecked"); // pure circle icon
+
+topPanel.addWidget("org.kde.plasma.appmenu");
+topPanel.addWidget("org.kde.plasma.panelspacer");
+topPanel.addWidget("org.kde.plasma.digitalclock");
+topPanel.addWidget("org.kde.plasma.panelspacer");
+topPanel.addWidget("org.kde.plasma.systemtray");
 
 var dock = new Panel("org.kde.panel");
 dock.location = "bottom";
@@ -26,4 +23,7 @@ dock.height = 48;
 dock.alignment = "center";
 dock.lengthMode = "fit";
 dock.floating = true;
-dock.addWidget("org.kde.plasma.icontasks");
+
+var tasks = dock.addWidget("org.kde.plasma.icontasks");
+tasks.currentConfigGroup = ["General"];
+tasks.writeConfig("launchers", "applications:org.kde.dolphin.desktop,applications:org.kde.konsole.desktop");
